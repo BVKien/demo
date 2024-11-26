e76cc059ff2841956608dca507ef006dc9795f08
KienBV <kbui0212@gmail.com>
2024-11-26 14:28:09 +0700

    up

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime? CreatedDate { get; set; }
+    }
+}
